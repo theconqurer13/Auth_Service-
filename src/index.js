8b98c8a6ffe65109d00ecaba7884b7dc7a2328ac
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-02-21 00:13:20 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/server-config')
 const apiRoutes = require('./routes/index')
 const app = express();
-
+const  db = require('./models/index');
 const prepareAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
@@ -12,6 +12,9 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT,()=>{
         console.log(`Server started on port : ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
     })
    
 
